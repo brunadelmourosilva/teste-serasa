5d86e259e02f3e21a481df4349e16d99ffd0f53d
Bruna Delmouro <brunadelmouro@gmail.com>
2023-12-23 22:36:03 -0300

    Initial structure

---
+++ Desafio Serasa Experian/src/main/java/br/com/brunadelmouro/apiserasa/DesafioSerasaExperianApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunadelmouro.apiserasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSerasaExperianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioSerasaExperianApplication.class, args);
+    }
+
+}
---
+++ Desafio Serasa Experian/src/test/java/br/com/brunadelmouro/apiserasa/DesafioSerasaExperianApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunadelmouro.apiserasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSerasaExperianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
