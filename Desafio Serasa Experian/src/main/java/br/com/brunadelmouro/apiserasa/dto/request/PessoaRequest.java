ac1f6279c003b0ff335fa9dfc31f9e746d635ced
Bruna Delmouro <brunadelmouro@gmail.com>
2023-12-29 20:22:41 -0300

    Implementing @Schema annotation

--- Desafio Serasa Experian/src/main/java/br/com/brunadelmouro/apiserasa/dto/request/PessoaRequest.java
+++ Desafio Serasa Experian/src/main/java/br/com/brunadelmouro/apiserasa/dto/request/PessoaRequest.java
@@ -2,12 +2,14 @@ package br.com.brunadelmouro.apiserasa.dto.request;
 
 import br.com.brunadelmouro.apiserasa.annotations.NoHyphen;
 import br.com.brunadelmouro.apiserasa.annotations.ScoreRange;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record PessoaRequest(
-    @NotBlank String nome,
-    @NotNull Integer idade,
-    @NotBlank String telefone,
-    @NotBlank @NoHyphen String cep,
-    @NotNull @ScoreRange Integer score) {}
+    @NotBlank @Schema(example = "Bruna") String nome,
+    @NotNull @Schema(example = "21") Integer idade,
+    @NotBlank @Schema(example = "999999999") String telefone,
+    @NotBlank @NoHyphen @Schema(example = "37500190")
+        String cep,
+    @NotNull @ScoreRange @Schema(example = "100") Integer score) {}
